de3342788a4ac5b5f12c67a670298b8c09fb49e8
yamanoi-shu <surayama1111@gmail.com>
2022-04-22 23:50:10 +0900

    gorilla 導入

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_simple_api
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	r := mux.NewRouter()
+	r.HandleFunc("/create", createHandler).Methods("POST")
+
+	http.ListenAndServe(":8080", r)
+}
+
+type User struct {
+	Name string
+}
+
+func createHandler(res http.ResponseWriter, req *http.Request) {
+	reqBody, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	var user User
+
+	err = json.Unmarshal(reqBody, &user)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s\n", user.Name)
 }
